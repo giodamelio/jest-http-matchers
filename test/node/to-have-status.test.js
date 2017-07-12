6f38a8f2f33e05f45492cfa996a131d9cc1c548f
Gio d'Amelio <giodamelio@gmail.com>
2017-07-11 22:44:23 -0700

    Test .toHaveStatus with 'node-fetch' module

--- test/node/to-have-status.test.js
+++ test/node/to-have-status.test.js
@@ -4,6 +4,7 @@ const { URL } = require('url');
 const axios = require('axios');
 const request = require('request');
 const superagent = require('superagent');
+const fetch = require('node-fetch');
 
 const httpMatchers = require('../../src');
 const createServer = require('./createServer');
@@ -43,6 +44,12 @@ describe('.toHaveStatus', () => {
 
     it('superagent', () => {
       return superagent.get(`${baseUrl()}/status/200`).then(response => {
+        expect(response).toHaveStatus(200);
+      });
+    });
+
+    it('fetch', () => {
+      return fetch(`${baseUrl()}/status/200`).then(response => {
         expect(response).toHaveStatus(200);
       });
     });
